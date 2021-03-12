9e64f06c7e0badf9b5422f89b3bc2e80343dcb57
Bao <quocbaole5420@gmail.com>
2021-03-12 18:20:27 +0700

    addall

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Carousel from './carousel/index'
+import Navbar from './navBar'
+import Smartphone from './smartphone'
+import Laptop from './laptop'
+import Promotion from './promotion'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Carousel />
+      <Smartphone />
+      <Laptop />
+      <Promotion />
     </div>
   );
 }
